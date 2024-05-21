9144470f852f13b0f7f681b26bd2961ea13bdf2b
Mark Nemmer <marknemmer@gmail.com>
2024-05-21 17:44:11 -0400

    Image updates.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image";
 import styles from '@/app/ui/home.module.css';
 
-
+/**
+ * The Home page component.
+ *
+ * @returns The Home page component JSX.
+ */
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className={`${styles.section} ${styles.first}`}>
+        <div className={styles.background}>
+          <Image
+            src="/HERO_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            priority
+          />
+        </div>
+
         <div className={styles.nycContainer}>
           <Image
             src="/$NYC_Logo.png"
@@ -19,7 +33,7 @@ export default function Home() {
         <div className={styles.gothamContainer}>
           <Image
             src="/Sol_Of_Gotham.png"
-            alt="$NYC Logo"
+            alt="The SOL of Gotham"
             layout="fill"
             objectFit="contain"
             priority
@@ -29,7 +43,7 @@ export default function Home() {
         <div className={styles.interboroContainer}>
           <Image
             src="/Interboro_Currency.svg"
-            alt="$NYC Logo"
+            alt="The only interboro currency"
             layout="fill"
             objectFit="contain"
             priority
@@ -38,6 +52,15 @@ export default function Home() {
       </div>
 
       <div className={`${styles.section} ${styles.second}`}>
+        <div className={`${styles.background}`}>
+          <Image
+            src="/SECTION2_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+
         <div className={styles.textContent}>
           <div className={styles.title}>
             Fuck,<br/> Another Memecoin?
@@ -70,42 +93,54 @@ export default function Home() {
       </div>
 
       <div className={`${styles.section} ${styles.third}`}>
-        <div className={`${styles.left}`}>
-          <div className={`${styles.description}`}>
-            <p>
-              SO….THIS PROJECT RUNS ON SOLANA.
-            </p>
-
-            <p>
-              IT’S THE FASTEST AND STURDIEST CHAIN.
-              NO REASON TO ARGUE. OR RESEARCH.
-              IT JUST IS.
-            </p>
-
-            <p>
-              GET A WALLET, BUY SOL AND THEN
-              SWAP IN SOME $NYC.
-            </p>
-
-            <p>
-              AND YEAH, YEAH, YEAH, DON’T WORRY.
-            </p>
-
-            <p>
-              YOU’RE STILL EARLY.
-            </p>
-
-            <p>
-              WE AIMING TO SCRAPE THAT FUCKING SKY.
-            </p>
-          </div>
+        <div className={`${styles.background}`}>
+          <Image
+            src="/SECTION3_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
 
-        <div className={`${styles.right}`}></div>
+        <div className={`${styles.description}`}>
+          <p>
+            SO….THIS PROJECT RUNS ON SOLANA.
+          </p>
+
+          <p>
+            IT’S THE FASTEST AND STURDIEST CHAIN.
+            NO REASON TO ARGUE. OR RESEARCH.
+            IT JUST IS.
+          </p>
+
+          <p>
+            GET A WALLET, BUY SOL AND THEN
+            SWAP IN SOME $NYC.
+          </p>
+
+          <p>
+            AND YEAH, YEAH, YEAH, DON’T WORRY.
+          </p>
+
+          <p>
+            YOU’RE STILL EARLY.
+          </p>
+
+          <p>
+            WE AIMING TO SCRAPE THAT FUCKING SKY.
+          </p>
+        </div>
       </div>
 
       <div className={`${styles.section} ${styles.fourth}`}>
-
+        <div className={`${styles.background}`}>
+          <Image
+            src="/SECTION4_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       </div>
 
       <div className={`${styles.section} ${styles.fifth}`}>
@@ -115,34 +150,47 @@ export default function Home() {
       </div>
 
       <div className={`${styles.section} ${styles.sixth}`}>
-
+        <div className={`${styles.background}`}>
+          <Image
+            src="/SECTION5_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
       </div>
 
       <div className={`${styles.section} ${styles.seventh}`}>
-        <div className={`${styles.left}`}></div>
+        <div className={`${styles.background}`}>
+          <Image
+            src="/SECTION6_BG.png"
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            objectPosition="left center"
+          />
+        </div>
 
-        <div className={`${styles.right}`}>
-          <div className={`${styles.textBlocks}`}>
-            <div>
-              YOU KNOW THE DEAL.
-              DO SOME SHIT.
-              CHANGE YOUR LIFE.
-              FOR STARTERS, HIT US UP.
-              ASK SOME QUESTIONS.
-            </div>
+        <div className={`${styles.textBlocks}`}>
+          <div>
+            YOU KNOW THE DEAL.
+            DO SOME SHIT.
+            CHANGE YOUR LIFE.
+            FOR STARTERS, HIT US UP.
+            ASK SOME QUESTIONS.
+          </div>
 
-            <div>
-              <p>
-                BTW, WE ONLY HAVE A TG.
-                DISCORD IS FOR GAMERS—
-                WE’RE BALLERS.
-              </p>
+          <div>
+            <p>
+              BTW, WE ONLY HAVE A TG.
+              DISCORD IS FOR GAMERS—
+              WE’RE BALLERS.
+            </p>
 
-              <p>
-                SEE YOU ON THE NEXT FLR
-                AS WE GET ELEVATED.
-              </p>
-            </div>
+            <p>
+              SEE YOU ON THE NEXT FLR
+              AS WE GET ELEVATED.
+            </p>
           </div>
         </div>
       </div>
