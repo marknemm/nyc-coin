3de184320784d1c90d53e61e32043f3a9d39dc37
Mark Nemmer <marknemmer@gmail.com>
2024-05-20 20:37:52 -0400

    Start

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,150 @@
 import Image from "next/image";
+import styles from '@/app/ui/home.module.css';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className={`${styles.section} ${styles.first}`}>
+        <div className={styles.nycImageContainer}>
+          <Image
+            src="/$NYC_Logo.png"
+            alt="$NYC Logo"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
+        </div>
+
+        <div className={styles.gothamImageContainer}>
+          <Image
+            src="/Sol_Of_Gotham.png"
+            alt="$NYC Logo"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
+        </div>
+
+        <div className={styles.interboroImageContainer}>
+          <Image
+            src="/Interboro_Currency.svg"
+            alt="$NYC Logo"
+            layout="fill"
+            objectFit="contain"
+            priority
+          />
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={`${styles.section} ${styles.second}`}>
+        <div className={styles.textContent}>
+          <div className={styles.title}>
+            Fuck,<br/> Another Memecoin?
+          </div>
+
+          <div className={styles.description}>
+            <p>
+              EH. NOT REALLY. WE DEVS OF $NYC,
+              ARE LEGIT NEW YORKERS. LIVE + WORK HERE.
+              THAT’S NO SHIT. GOT THE DL TO PROVE IT.
+            </p>
+
+            <p>
+              WE’RE BUILDING A PROJECT, FOR REALS.
+              BEYOND A CORNY, HACK PHOTOSHOP JOB OR
+              HOT, DUMB NARRATIVE OF THE MOMENT.
+            </p>
+
+            <p>
+              THIS INITIATIVE SPEAKS TO THE CALIBER OF
+              CITY THAT WE LOVE. THE CITY THAT’S BETTER
+              THAN EVERY OTHER ONE.
+            </p>
+
+            <p>
+              $NYC—THE CITY THAT PLAYS FOR KEEPS.
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className={`${styles.section} ${styles.third}`}>
+        <div className={`${styles.left}`}>
+          <div className={`${styles.description}`}>
+            <p>
+              SO….THIS PROJECT RUNS ON SOLANA.
+            </p>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+            <p>
+              IT’S THE FASTEST AND STURDIEST CHAIN.
+              NO REASON TO ARGUE. OR RESEARCH.
+              IT JUST IS.
+            </p>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <p>
+              GET A WALLET, BUY SOL AND THEN
+              SWAP IN SOME $NYC.
+            </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <p>
+              AND YEAH, YEAH, YEAH, DON’T WORRY.
+            </p>
+
+            <p>
+              YOU’RE STILL EARLY.
+            </p>
+
+            <p>
+              WE AIMING TO SCRAPE THAT FUCKING SKY.
+            </p>
+          </div>
+        </div>
+
+        <div className={`${styles.right}`}></div>
+      </div>
+
+      <div className={`${styles.section} ${styles.fourth}`}>
+
+      </div>
+
+      <div className={`${styles.section} ${styles.fifth}`}>
+        THE STRUGGLE IS REAL. PROJECTS ARE MESSY. AND WE CONTINUE BECAUSE IT MATTERS TOO MUCH TO US.
+        $NYC WILL EVENTUALLY BE USED TO BUY QUALITY GOODS FROM YOUR HARD-BOILED, ALL-BORO KINGS HERE
+        AT THE HELM. SHIT THAT WILL ONLY BE AVAILABLE HERE AND FROM US. NO NFTS. JUST REAL SHIT. TRUST.
+      </div>
+
+      <div className={`${styles.section} ${styles.sixth}`}>
+
+      </div>
+
+      <div className={`${styles.section} ${styles.seventh}`}>
+        <div className={`${styles.left}`}></div>
+
+        <div className={`${styles.right}`}>
+          <div className={`${styles.textBlocks}`}>
+            <div>
+              YOU KNOW THE DEAL.
+              DO SOME SHIT.
+              CHANGE YOUR LIFE.
+              FOR STARTERS, HIT US UP.
+              ASK SOME QUESTIONS.
+            </div>
+
+            <div>
+              <p>
+                BTW, WE ONLY HAVE A TG.
+                DISCORD IS FOR GAMERS—
+                WE’RE BALLERS.
+              </p>
+
+              <p>
+                SEE YOU ON THE NEXT FLR
+                AS WE GET ELEVATED.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
