e8080c9eb75c24cd38e0e6bea74b1fe929fa7b64
Mark Nemmer <marknemmer@gmail.com>
2024-05-22 15:17:27 -0400

    Add some simple animations.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import styles from '@/app/ui/home.module.css';
 import Image from "next/image";
+import { Fade } from 'react-awesome-reveal';
 
 /**
  * The {@link Home} page component.
@@ -58,35 +61,40 @@ export default function Home() {
           />
         </div>
 
-        <div className={styles.textContent}>
-          <div className={styles.title}>
-            Fuck,<br/> Another Memecoin?
+        <Fade
+          direction="right"
+          triggerOnce
+        >
+          <div className={`${styles.textContent} ${styles.animateLeft}`}>
+            <div className={styles.title}>
+              Fuck,<br/> Another Memecoin?
+            </div>
+
+            <div className={styles.description}>
+              <p>
+                EH. NOT REALLY. WE DEVS OF $NYC,
+                ARE LEGIT NEW YORKERS. LIVE + WORK HERE.
+                THAT’S NO SHIT. GOT THE DL TO PROVE IT.
+              </p>
+
+              <p>
+                WE’RE BUILDING A PROJECT, FOR REALS.
+                BEYOND A CORNY, HACK PHOTOSHOP JOB OR
+                HOT, DUMB NARRATIVE OF THE MOMENT.
+              </p>
+
+              <p>
+                THIS INITIATIVE SPEAKS TO THE CALIBER OF
+                CITY THAT WE LOVE. THE CITY THAT’S BETTER
+                THAN EVERY OTHER ONE.
+              </p>
+
+              <p>
+                $NYC—THE CITY THAT PLAYS FOR KEEPS.
+              </p>
+            </div>
           </div>
-
-          <div className={styles.description}>
-            <p>
-              EH. NOT REALLY. WE DEVS OF $NYC,
-              ARE LEGIT NEW YORKERS. LIVE + WORK HERE.
-              THAT’S NO SHIT. GOT THE DL TO PROVE IT.
-            </p>
-
-            <p>
-              WE’RE BUILDING A PROJECT, FOR REALS.
-              BEYOND A CORNY, HACK PHOTOSHOP JOB OR
-              HOT, DUMB NARRATIVE OF THE MOMENT.
-            </p>
-
-            <p>
-              THIS INITIATIVE SPEAKS TO THE CALIBER OF
-              CITY THAT WE LOVE. THE CITY THAT’S BETTER
-              THAN EVERY OTHER ONE.
-            </p>
-
-            <p>
-              $NYC—THE CITY THAT PLAYS FOR KEEPS.
-            </p>
-          </div>
-        </div>
+        </Fade>
       </div>
 
       <div className={`${styles.section} ${styles.third}`}>
@@ -99,32 +107,39 @@ export default function Home() {
         </div>
 
         <div className={`${styles.description}`}>
-          <p>
-            SO….THIS PROJECT RUNS ON SOLANA.
-          </p>
+          <Fade
+            direction="left"
+            triggerOnce
+          >
+            <div>
+              <p>
+                SO….THIS PROJECT RUNS ON SOLANA.
+              </p>
 
-          <p>
-            IT’S THE FASTEST AND STURDIEST CHAIN.
-            NO REASON TO ARGUE. OR RESEARCH.
-            IT JUST IS.
-          </p>
+              <p>
+                IT’S THE FASTEST AND STURDIEST CHAIN.
+                NO REASON TO ARGUE. OR RESEARCH.
+                IT JUST IS.
+              </p>
 
-          <p>
-            GET A WALLET, BUY SOL AND THEN
-            SWAP IN SOME $NYC.
-          </p>
+              <p>
+                GET A WALLET, BUY SOL AND THEN
+                SWAP IN SOME $NYC.
+              </p>
 
-          <p>
-            AND YEAH, YEAH, YEAH, DON’T WORRY.
-          </p>
+              <p>
+                AND YEAH, YEAH, YEAH, DON’T WORRY.
+              </p>
 
-          <p>
-            YOU’RE STILL EARLY.
-          </p>
+              <p>
+                YOU’RE STILL EARLY.
+              </p>
 
-          <p>
-            WE AIMING TO SCRAPE THAT FUCKING SKY.
-          </p>
+              <p>
+                WE AIMING TO SCRAPE THAT FUCKING SKY.
+              </p>
+            </div>
+          </Fade>
         </div>
       </div>
 
@@ -139,9 +154,13 @@ export default function Home() {
       </div>
 
       <div className={`${styles.section} ${styles.fifth}`}>
-        THE STRUGGLE IS REAL. PROJECTS ARE MESSY. AND WE CONTINUE BECAUSE IT MATTERS TOO MUCH TO US.
-        $NYC WILL EVENTUALLY BE USED TO BUY QUALITY GOODS FROM YOUR HARD-BOILED, ALL-BORO KINGS HERE
-        AT THE HELM. SHIT THAT WILL ONLY BE AVAILABLE HERE AND FROM US. NO NFTS. JUST REAL SHIT. TRUST.
+        <Fade triggerOnce>
+          <div>
+            THE STRUGGLE IS REAL. PROJECTS ARE MESSY. AND WE CONTINUE BECAUSE IT MATTERS TOO MUCH TO US.
+            $NYC WILL EVENTUALLY BE USED TO BUY QUALITY GOODS FROM YOUR HARD-BOILED, ALL-BORO KINGS HERE
+            AT THE HELM. SHIT THAT WILL ONLY BE AVAILABLE HERE AND FROM US. NO NFTS. JUST REAL SHIT. TRUST.
+          </div>
+        </Fade>
       </div>
 
       <div className={`${styles.section} ${styles.sixth}`}>
--- app/ui/header/header.hooks.ts
+++ app/ui/header/header.hooks.ts
@@ -3,23 +3,24 @@ import { Dispatch, SetStateAction, useEffect, useState } from 'react';
 /**
  * Custom hook that gets and maintains the sticky state for the `Header`.
  *
- * @param initSticky The initial sticky state. Defaults to `false`.
+ * @param threshold The threshold `scrollY` value for the sticky state. Defaults to the `window.innerHeight`.
  * @returns The sticky state and the setter for the sticky state.
  */
-export function useStickyState(initSticky = false): [boolean, Dispatch<SetStateAction<boolean>>] {
-  const [sticky, setSticky] = useState(initSticky);
+export function useStickyState(threshold?: number): [boolean, Dispatch<SetStateAction<boolean>>] {
+  const [sticky, setSticky] = useState(false);
 
   useEffect(() => {
-    const scrollCb = () => {
-      setSticky(window.scrollY >= window.innerHeight);
+    const computeSticky = () => {
+      setSticky(window.scrollY >= (threshold ?? window.innerHeight));
     }
 
-    window.addEventListener('scroll', scrollCb);
+    computeSticky();
+    window.addEventListener('scroll', computeSticky);
 
     return () => {
-      window.removeEventListener('scroll', scrollCb);
+      window.removeEventListener('scroll', computeSticky);
     }
-  }, []);
+  }, [threshold]);
 
   return [sticky, setSticky];
 }
